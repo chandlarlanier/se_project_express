9b53cf0cd5d8819e793ad4dd5a4d2187e79a98ab
Chandlar Lanier <cklanier@outlook.com>
2023-09-21 19:09:06 -0400

    Schema

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (r) => {
+  console.log('Connected to DB', r);
+}, (e) => {
+  console.log('DB error', e);
+});
+
+const routes = require('./routes');
+app.use(routes);
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
