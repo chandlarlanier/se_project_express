5bd867e6d761299b119ffac4aaba92bcc8860b3f
Chandlar Lanier <cklanier@outlook.com>
2023-09-22 15:14:38 -0400

    Create controllers/routes for clothing items

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (r) => {
 });
 
 const routes = require('./routes');
+app.use(express.json());
 app.use(routes);
-app.use(express.json())
+
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,20 +1,57 @@
-const ClothingItem = require('../models/clothingItem')
+const ClothingItem = require("../models/clothingItem");
 
 const createItem = (req, res) => {
   console.log(req);
-  console.log(req.body)
+  console.log(req.body);
 
-  const {name, weather, imageURL} = req.body;
+  const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({name, weather, imageURL}).then((item) => {
-    console.log(item);
-    res.send({data: item})
-  }).catch((e) => {
-    res.status(500).send({message: 'Error from createItem', e})
-  })
-}
+  ClothingItem.create({ name, weather, imageUrl })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from createItem", e });
+    });
+};
 
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => {
+      res.status(200).send(items);
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from updateItem", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  console.log(itemId);
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from deleteItem", e });
+    });
+};
 
 module.exports = {
-  createItem
-}
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem
+};
