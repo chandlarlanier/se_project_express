52453eaa974204c9d0781722ac838b67b6b2d4d6
Chandlar Lanier <cklanier@outlook.com>
2023-10-16 21:41:14 -0400

    Update createUser controller to include email and password

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,38 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const { handleError } = require("../utils/errors");
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((error) => {
-      handleError(req, res, error);
+  User.findOne({ email }).then((data) => {
+    if (data) {
+      //need to update error code
+      return res.status(409).send({ message: "Email already exists" });
+    }
+    return bcrypt.hash(password, 10).then((hash) => {
+      User.create({ name, avatar, email, password: hash }).then((user) => {
+        res.send({
+          data: {
+            _id: user._id,
+            name: user.name,
+            avatar: user.avatar,
+            email: user.email,
+          },
+        });
+      }).catch((e) => {
+        handleError(req, res, e);
+      });
     });
+  });
+
+  // User.create({ name, avatar })
+  //   .then((user) => {
+  //     res.send(user);
+  //   })
+  //   .catch((error) => {
+  //     handleError(req, res, error);
+  //   });
 };
 
 const getUsers = (req, res) => {
