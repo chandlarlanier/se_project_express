202a1363bb4fd5dd6277964dc9723cab8099e275
Chandlar Lanier <cklanier@outlook.com>
2023-10-21 21:47:20 -0400

    Add user.statics in user schema

--- controllers/users.js
+++ controllers/users.js
@@ -35,8 +35,7 @@ const createUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
 
-  return User.findUserByCredentials({ email })
-    .select("+password")
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       res.send({
         token: jwt.sign(
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const user = new mongoose.Schema({
   name: {
@@ -35,5 +36,26 @@ const user = new mongoose.Schema({
     select: false,
   },
 });
+
+user.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return bcrypt.compare(password, user.password).then((foundUser) => {
+        if (!foundUser) {
+          return Promise.reject(new Error("Incorrect email or password"))
+        }
+
+        return user;
+      })
+    });
+};
 
 module.exports = mongoose.model("users", user);
